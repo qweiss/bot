494e8d3d07ca658140908364c02450ac18704f73
qweiss <165024007+qweiss@users.noreply.github.com>
2024-12-28 20:46:26 +0500

    Update run.py

--- run.py
+++ run.py
@@ -5,10 +5,6 @@ room_id = "630b3ee87e9f9cf7d9257c4a"
 bot_token = "3d3e7f3dea69d81c8e3f0714a06de30f165c9721bfcdde89ef06e082c9de1f98"
 bot_file = "main"
 bot_class = "MyBot"
-coordinates = {
-        'x': 12,
-        'y': 0.5,
-        'z': 7,
 
 if __name__ == "__main__":
   definitions = [
