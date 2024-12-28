8b5d22a11070d1599430a826fc236d87743c283c
qweiss <165024007+qweiss@users.noreply.github.com>
2024-12-28 20:41:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,4 @@ from highrise import BaseBot, SessionMetadata
 class MyBot(BaseBot): 
     async def on_start(self, session_metadata: SessionMetadata) -> None:
         print("Bot started")
-        coordinates = {
-        'x': 12,
-        'y': 0.5,
-        'z': 7,
+       
